dd7302fc880c158ffffada2aac23e56fca118a48
Helgik <36006217+Helgik@users.noreply.github.com>
2018-02-15 23:47:59 +0200

    Added NavigationHelper class, refactoring

--- addressbook-web-tests/src/test/java/olga/javatasks/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/olga/javatasks/addressbook/tests/GroupCreationTests.java
@@ -7,7 +7,7 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
-        app.goToGroupPage();
+        app.getNavigationHelper().goToGroupPage();
         app.getGroupHelper().initGroupCreation();
         app.getGroupHelper().fillGroupForm(new GroupData("some info", "blabla", "more info"));
         app.getGroupHelper().submitGroupForm();
