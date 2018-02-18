b83d8647e564e33004ad9951b95e5213099d2d8f
Helgik <36006217+Helgik@users.noreply.github.com>
2018-02-18 20:13:07 +0200

    Refactoring of GroupHelper, created type and click methods

--- addressbook-web-tests/src/test/java/olga/javatasks/addressbook/tests/AddNewContact.java
+++ addressbook-web-tests/src/test/java/olga/javatasks/addressbook/tests/AddNewContact.java
@@ -8,7 +8,7 @@ public class AddNewContact extends TestBase {
     @Test
     public void AddNewContact() {
         app.getNavigationHelper().goToNewContactCreationPage();
-        app.getContactHelper().fillNewContactForm(new NewContactData("Arnold", "Schvarzenegger", "LA", "+00449794648", "arni@schvarz.com"));
+        app.getContactHelper().fillNewContactForm(new NewContactData("Hello", "World", "none", "+12345", "email@gmail.com"));
         app.getContactHelper().submitNewContactCreationForm();
         app.getContactHelper().returnToAddressbookPage();
     }
